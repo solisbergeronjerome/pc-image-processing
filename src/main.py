771d748244ea65f316db633a1b18946c9df17673
solisbergeronjerome <solisbergeron.jerome@gmail.com>
2026-05-04 16:17:51 -0400

    Scaling function implemented

--- src/main.py
+++ src/main.py
@@ -1,4 +1,28 @@
-def main():
+import cv2 as cv
 
-    if "no":
-        pass
+defaultRes = (920, 920)
+
+img = cv.imread("Picture.png")
+
+
+def reScaleImage(image, res=defaultRes):
+    """Scales an image up or down to the default
+        resolution set by defaultRes
+        (INTER_AREA for downscale).
+
+    Args:
+        image (cv): image to upscale or downscale
+
+    Returns:
+        scaledImage: Image set to the default scale
+    """
+    height, width = image.shape[:2]
+    if height < res[0] | width < res[1]:
+        scaledImage = cv.resize(image, res, interpolation=cv.INTER_AREA)
+    else:
+        scaledImage = cv.resize(image, res, interpolation=cv.INTER_LINEAR)
+    return scaledImage
+
+
+cv.imshow("Camera Feed", reScaleImage(img))
+k = cv.waitKey(0)
